cc80976166bfc3fb6ef7a138daac708e7bff9649
KojiroSchatten <banbirou@icloud.com>
2021-09-04 14:23:31 +0900

    add: route process

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
-// import { Header } from './components/header/Header'
-// import { Footer } from './components/footer/Footer'
 import { HeroPage } from './components/pages/HeroPage'
+import { AboutPage } from './components/pages/AboutPage'
+import { Navbar } from './components/header/Navbar'
+const BrowserRouter = require('react-router-dom').BrowserRouter
+const Route = require('react-router-dom').Route
 
 function App() {
   return (
     <div className="App">
-      <HeroPage />
+      <BrowserRouter>
+        <Navbar />
+        {/* ここでのHeroは後々/heroに行かせる？？ */}
+        <HeroPage />
+        {/* <AboutPage /> */}
+        <Route exact path="/home" component={HeroPage} />
+        <Route exact path="/about" component={AboutPage} />
+      </BrowserRouter>
     </div>
   )
 }
--- src/components/pages/HeroPage.tsx
+++ src/components/pages/HeroPage.tsx
@@ -4,10 +4,13 @@ import { Header } from '../header/Header'
 import { Hero } from '../hero/Hero'
 import { SideBar } from '../side/SideBar'
 
+const Link = require('react-router-dom').Link
+
 export const HeroPage = () => {
   return (
     <div>
       <Header />
+      <Link to="/about">LINK TO ABOUT</Link>
       <Hero />
       <SideBar />
       <Footer />
